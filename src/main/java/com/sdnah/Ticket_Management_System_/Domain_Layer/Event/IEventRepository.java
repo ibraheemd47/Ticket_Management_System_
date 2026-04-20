9574640c36c3aeaf06dc4f0e552d07234206682a
sarismo <mohamadsaris913@gmail.com>
2026-04-20 17:26:08 +0300

    init classes fields and also added the Waiting_Queue folder alsoadded with class QueueEntry Class

--- src/main/java/com/sdnah/Ticket_Management_System_/Domain_Layer/Event/IEventRepository.java
+++ src/main/java/com/sdnah/Ticket_Management_System_/Domain_Layer/Event/IEventRepository.java
@@ -1,5 +1,9 @@
-package Event;
+package com.sdnah.Ticket_Management_System_.Domain_Layer.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface IEventRepository {
-
+@Repository
+public interface IEventRepository extends JpaRepository<Event, Long> {
+    // TODO : Implement the queries for the Event entity here
+    // we still don't know if we have to put the Repository in the domain Layer or insturfracture layer.
 }
