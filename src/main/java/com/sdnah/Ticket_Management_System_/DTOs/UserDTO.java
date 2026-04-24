dcb17f12266aa135ad04cbdd1ed9d6c26d3676a7
Zaki Masarwe <zakimas@post.bgu.ac.il>
2026-04-24 18:37:57 +0300

    fix userdto

--- src/main/java/com/sdnah/Ticket_Management_System_/DTOs/UserDTO.java
+++ src/main/java/com/sdnah/Ticket_Management_System_/DTOs/UserDTO.java
@@ -4,6 +4,9 @@ public class UserDTO {
 	private String username;
 	private String password;
 	private String token;
+	private String email;
+	private String code;
+	private String newPassword;
 
 	public UserDTO() {
 	}
@@ -30,6 +33,30 @@ public class UserDTO {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
 	}
 
 }
